ddc5f23ba58bad7d2394ce677e76071590525517
imzachjohnson <imzachjohnson@gmail.com>
2021-09-16 18:05:18 -0700

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
 
 # -- Project information -----------------------------------------------------
 
